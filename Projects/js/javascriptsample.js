0cd7e96be4c17ddf53177e119f8d2d2bec906769
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2018-08-27 20:39:33 -0500

    Chapter 2

--- Projects/js/javascriptsample.js
+++ Projects/js/javascriptsample.js
@@ -1,3 +1,6 @@
+/* Th i s script displays a greeting to the user based upon the current time.
+It is an example from JavaScript & jQuery book */
+
 var today= new Date();
 var hourNow = today.getHours();
 var greeting;
@@ -18,4 +21,15 @@ else {
 greeting = 'Welcome! ' ;
 }
 
-document .write( ' <h3>' +greeting + ' </ h3>');
+document.write( ' <h3>' +greeting + ' </ h3>');
+
+var ed = document.getElementById('god');
+ed.textContent = 'Narayana' + 1;
+
+var samplemessage = new String("sf\nddg\"sdg\" se");
+document.write('<br />' + samplemessage);
+
+var colors;
+colors = ['white', 'black', ' custom ']; /* Or colors = new Array('white', 'black', 'custom');*/
+
+document.write(colors[2]);
