1e59a55dc789e4d91c21074b6e89efbd3f9224b8
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2018-08-30 20:52:05 -0500

    minor change

--- Projects/js/sibling.js
+++ Projects/js/sibling.js
@@ -1,5 +1,9 @@
 // Pg 229, 231
 
+var itemTwo = document.getElementById('two');
+var elText = itemTwo.firstChild.nodeValue;
+elText = elText.replace('pine nuts', 'kale' );
+itemTwo.firstChild.nodeValue = elText;
 
 // Select the starting point and find its siblings.
 var startItem = document.getElementById('two');
