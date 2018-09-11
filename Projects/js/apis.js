45c6d57b78ca11ce45c1b74e1f4c0d13b683457d
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2018-09-11 12:16:09 -0500

    AngularJS basics and APIs

---
+++ Projects/js/apis.js
@@ -0,0 +1,42 @@
+var elr = document.getElementById('df');
+
+if(Modernizr.geolocation){
+  navigator.geolocation.getCurrentPosition(success, fail);
+}else{
+  elr.textContent = "Sorry we could not get your location";
+}
+var msg;
+function success(position){
+  msg = '<h3>Latitude: </h3>';
+  msg += position.coords.latitude + '<br>';
+  msg += '<h3>Longitude: </h3>';
+  msg += position.coords.longitude;
+  elr.innerHTML = msg;
+}
+
+function fail(msgError){
+  elr.textContent = msgError;
+  console.log(msgError.code);
+}
+
+function GreetingController($scope){
+  $scope.greeting = 'dfs';
+}
+
+var des = document.getElementById('username');
+
+if(window.localStorage){
+  window.localStorage.setItem('age', 23); // or localstorage.age = 23, Similarly for getItem as well
+  des.addEventListener('input', function(){
+    localStorage.username = des.value;
+  }, false);
+}
+
+// sessionStorage is just like localStorage
+var location2 = location.pathname;
+var page = location2.substring(location2.lastIndexOf('/') + 1);
+
+window.addEventListener('popstate', function(){
+  var path = location.pathname;
+  var page = path.substring(path.lastIndexOf('/') + 1);
+}, false);
